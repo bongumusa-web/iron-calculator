bcedf62fd02c8c65accfacc3ebd9363dc56034a5
Bongumusa Gumede <bonggums@gmail.com>
2025-07-28 19:02:46 +0200

    iron-calculator

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-function calculatePeriod() {
+function calculatePeriod() { 
   const C_sol = 900;
   const C_wat = 4200;
   const L = 2264700;
   const k = 0.046;
   const x = 0.001;
-  const T_max = 210;
-  const T_min = 180;
-  const T_ambient = 25;
-  const T_avg = 195;
   const T_w1 = 25;
   const T_w2 = 100;
 
@@ -15,6 +11,10 @@ function calculatePeriod() {
   const mSol = parseFloat(document.getElementById('mSol').value);
   const mDot = parseFloat(document.getElementById('mDot').value);
   const area = parseFloat(document.getElementById('area').value);
+  const T_max = parseFloat(document.getElementById('Tmax').value);
+  const T_min = parseFloat(document.getElementById('Tmin').value);
+  const T_ambient = parseFloat(document.getElementById('Tambient').value);
+  const T_avg = (T_max + T_min) / 2;
 
   const Q_loss = mSol * C_sol * (T_max - T_min);
   const Q_out = (-k * area * (T_ambient - T_avg)) / x + mDot * C_wat * (T_w2 - T_w1) + mDot * L;
